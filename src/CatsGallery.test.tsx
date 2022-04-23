ab4ee86c4a98414dd08b8547863b0b9f49a67382
lockey33 <lockryptorentier@gmail.com>
2022-04-23 12:41:01 +0200

    fix: resolve test issue

--- src/CatsGallery.test.tsx
+++ src/CatsGallery.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CatsGallery from "./components/Gallery/CatsGallery";
+import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<CatsGallery />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
